1e6e0ea525ad69367b3b8f023057ad60c0d00fef
monsosam <monsonsamuel5@gmail.com>
2024-02-13 23:06:57 -0700

    pd

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -138,7 +138,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(deleted => {
+    if (deleted) {
+      res.json({ message: 'Product deleted successfully.' });
+    } else {
+      res.status(404).json({ message: 'No product found with this id' });
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
