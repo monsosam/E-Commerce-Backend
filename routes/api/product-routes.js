e88e302c66c13ef3b0225dadc05e439177f04a6c
monsosam <monsonsamuel5@gmail.com>
2024-03-02 17:32:37 -0700

    tiny comment edit

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -67,7 +67,7 @@ router.post('/', (req, res) => {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
-      tagIds: [1, 2, 3, 4]
+      tag_id: [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
